633a4fbc1f0067bc11de274107a08dd48543c15a
rogeriofourdev <chatbot.ti.developer@gmail.com>
2023-06-05 21:26:06 -0300

    feat: add msg console

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 
 const app = express();
-app.use(bodyParser.json());
-app.get('/', (req, res) => res.send('Hello World'));
+// app.use(bodyParser.json());
+app.use((req, res, next) => {
+    console.log('Will run before any route');
+    next();
+})
+app.get('/', function (req, res, next) {
+	console.log('route / called');
+	res.send('Hello World!');
+});
 
 app.listen(3000, () => {
 	console.log('listening on port 3000');
